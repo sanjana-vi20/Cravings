e30fb8790aa4d03ca9a7c2ad00bd487779432a90
sanjana-vi20 <vishwakarmasunita963@gmail.com>
2026-05-21 17:38:52 +0530

    fix: Update SOCKET_URL to use environment variable for backend configuration

--- client/src/config/Websocket.jsx
+++ client/src/config/Websocket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:4500"; // Tumhara backend port
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL; // Tumhara backend port
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false, // Isse hum apni marzi se connect karenge (jaise login ke baad)
